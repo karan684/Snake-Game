ed85a361f75d39241a53898b187dbccbf7fe1525
karan684 <51419129+karan684@users.noreply.github.com>
2022-07-04 09:26:15 +0530

    added an high score system

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.color("white")
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.goto(0, 270)
         self.score = 0
@@ -16,12 +18,20 @@ class Scoreboard(Turtle):
     def update_score(self):
         self.score += 1 #-my method for scorecard
         # wrote this later because score was flashing slowly
-        self.clear()
         self.scorecard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALINGMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as data:
+                data.write(str(self.high_score))
+        self.score = 0
+        self.scorecard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALINGMENT, font=FONT)
 
     def scorecard(self):
-        self.write(f"Score: {self.score}", align=ALINGMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALINGMENT, font=FONT)
